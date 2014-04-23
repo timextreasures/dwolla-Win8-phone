adc5c272aeca607b9d6b89738f20457e4b86165d
chrisrouw <chris@farreachinc.com>
2014-04-23 10:56:18 -0500

    Initial Commit for Project

---
+++ DwollaWP8SDKSamples/DwollaWP8SDKSamples/DwollaSDK/UcSendMoney.xaml.cs
@@ -0,0 +1,384 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using Dwolla.InAppSDK;
+using Dwolla.InAppSDK.Models;
+
+namespace DwollaInAppWP8.DwollaSDK
+{
+    public partial class UcSendMoney
+    {
+        #region Events
+
+        /// <summary>
+        /// Event fires when user clicks the cancel button
+        /// </summary>
+        public event EventHandler<string> CancelSendMoney;
+
+        /// <summary>
+        /// Event fires when user clicks the done button after money has been sent.
+        /// </summary>
+        public event EventHandler<string> SendMoneyCompleted;
+
+        #endregion
+
+        #region Private Properties
+
+        private SendMoneyHelper _sendMoneyHelper;
+        private double _amount;
+        private Account _merchantAccount;
+        private Account _account;
+
+        #endregion
+
+        #region Page Methods
+
+        public UcSendMoney()
+        {
+            InitializeComponent();
+            Loaded += ucSendMoney_Loaded;
+        }
+
+        private void ucSendMoney_Loaded(object sender, RoutedEventArgs e)
+        {
+            InitSteps();
+            _merchantAccount = null;
+            _account = null;
+            _amount = 0;
+        }
+
+        private void ButtonCancel_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (CancelSendMoney != null)
+                CancelSendMoney(this, null);
+        }
+
+        private async void ButtonNext_OnClick(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            if (button != null && button.Tag != null)
+            {
+                switch (button.Tag.ToString().ToLower())
+                {
+                    case "next":
+                        if (ValidateForm())
+                            ShowReview();
+                        break;
+                    case "edit":
+                        InitSteps();
+                        break;
+                    case "submit":
+                        await SendMoney();
+                        break;
+                    case "done":
+                        if (SendMoneyCompleted != null)
+                            SendMoneyCompleted(this, TransactionId);
+                        break;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Initializes the Send Money user control to be ready to process the Dwolla payment
+        /// </summary>
+        /// <param name="sendMoneyHelper">The current instance of the SendMoneyHelper class</param>
+        /// <param name="amount">The amount to charge</param>
+        public void Initialize(SendMoneyHelper sendMoneyHelper, double amount)
+        {
+            _sendMoneyHelper = sendMoneyHelper;
+            _amount = amount;
+            TxtAmount.Text = amount.ToString("C2");
+
+            InitSteps();
+            LoadMerchantInfo();
+            LoadFundsSource();
+            LoadBalance();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Initializes the Grids to show step 1.
+        /// </summary>
+        private void InitSteps()
+        {
+            TxtTitle.Text = "Send Money";
+            GrdSendMoneyInputs.Visibility = Visibility.Visible;
+            GrdReview.Visibility = Visibility.Collapsed;
+            GrdConfirm.Visibility = Visibility.Collapsed;
+        }
+
+        /// <summary>
+        /// Gets the account information for the Merchant
+        /// </summary>
+        private async void LoadMerchantInfo()
+        {
+            try
+            {
+                //Load the Merchant Information
+                var merchantAccount = await _sendMoneyHelper.GetMerchantAccount();
+                if (merchantAccount != null)
+                {
+                    _merchantAccount = merchantAccount;
+                    TxtMerchantName.Text = merchantAccount.Name;
+                    TxtMerchantId.Text = merchantAccount.Id;
+                    var merchantUri = new Uri(merchantAccount.Avatar);
+                    ImgMerchant.Source = new BitmapImage(merchantUri);
+                }
+
+                var account = await _sendMoneyHelper.GetAccount();
+                if (account != null)
+                {
+                    _account = account;
+                }
+
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        /// <summary>
+        /// Gets the funding sources for the authenticated user
+        /// </summary>
+        private async void LoadFundsSource()
+        {
+            try
+            {
+                //Load the funding sources dropdown
+                CboFundsSource.ItemsSource = null;
+                var fundsSources = await _sendMoneyHelper.GetAccountFundingSources();
+                if (fundsSources != null)
+                {
+                    var funds = fundsSources.ToList();
+                    CboFundsSource.ItemsSource = funds;
+                    CboFundsSource.SelectedItem = funds.FirstOrDefault();
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        /// <summary>
+        /// Gets the dwolla balance for the authenticated user
+        /// </summary>
+        private async void LoadBalance()
+        {
+            try
+            {
+                var balanceResponse = await _sendMoneyHelper.GetBalance();
+                if (balanceResponse != null)
+                {
+                    double balance;
+                    Double.TryParse(balanceResponse.Response, out balance);
+                    TxtAvailableDwollaBalance.Text = "Available Dwolla balance: " + balance.ToString("C2");
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        /// <summary>
+        /// Show the review form with the information for the transaction
+        /// </summary>
+        private void ShowReview()
+        {
+            try
+            {
+                BtnSendMoney.IsEnabled = true;
+
+                TxtTitle.Text = "Review";
+                TxtReviewAmount.Text = "send " + _amount.ToString("C2");
+
+                string destinationName = "";
+
+                //Image for the Merchant
+                if (_merchantAccount != null)
+                {
+                    var merchantUri = new Uri(_merchantAccount.Avatar);
+                    ImgDestination.Source = new BitmapImage(merchantUri);
+                    destinationName = _merchantAccount.Name;
+                }
+
+                //Image for the Authenticated User
+                if (_account != null)
+                {
+                    var userUri = new Uri(_account.Avatar);
+                    ImgSender.Source = new BitmapImage(userUri);
+                }
+
+                TxtReviewSend.Text = "You are about to send money to " + destinationName + " via " +
+                     GetSelectedFundSource() + ".";
+
+                TxtReviewSendTo.Text = destinationName;
+                TxtReviewSource.Text = GetSelectedFundSource();
+                TxtReviewAmount2.Text = _amount.ToString("C2");
+
+                GrdSendMoneyInputs.Visibility = Visibility.Collapsed;
+                GrdReview.Visibility = Visibility.Visible;
+                GrdConfirm.Visibility = Visibility.Collapsed;
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        /// <summary>
+        /// Gets the name of the selected funding source
+        /// </summary>
+        /// <returns>Name of the selected funding source</returns>
+        private string GetSelectedFundSource()
+        {
+            if (CboFundsSource.SelectedItem != null)
+            {
+                var fundSource = CboFundsSource.SelectedItem as FundingSource;
+                if (fundSource != null)
+                {
+                    return fundSource.Name;
+                }
+            }
+            return "";
+        }
+
+        /// <summary>
+        /// Call to the Send money API
+        /// </summary>
+        private async Task SendMoney()
+        {
+            //Call the send money
+            int pin;
+            if (Int32.TryParse(TxtPin.Password, out pin))
+            {
+                //Disable button so we don't click it twice.
+                BtnSendMoney.IsEnabled = false;
+
+                string fundsSource = "";
+                var fundSelected = CboFundsSource.SelectedItem as FundingSource;
+                if (fundSelected != null)
+                {
+                    fundsSource = fundSelected.Id;
+                }
+                var transaction = await _sendMoneyHelper.SendMoney(_amount, pin, fundsSource);
+                if (transaction.Success)
+                {
+                    TransactionId = transaction.TransactionId;
+                    //Show the confirmation screen
+                    LoadConfirm();
+                }
+                else
+                {
+                    //Error - Show the initial screen
+                    var result = MessageBox.Show("Error Sending Money: " + transaction.Message);
+                    if (result == MessageBoxResult.OK)
+                    {
+                        InitSteps();
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Show the confirmation screen
+        /// </summary>
+        private void LoadConfirm()
+        {
+            try
+            {
+                string destinationName = "";
+
+                //Image for the Merchant
+                if (_merchantAccount != null)
+                {
+                    var merchantUri = new Uri(_merchantAccount.Avatar);
+                    ImgDestinationConfirm.Source = new BitmapImage(merchantUri);
+                    destinationName = _merchantAccount.Name;
+                }
+
+                //Image for the Authenticated User
+                if (_account != null)
+                {
+                    var userUri = new Uri(_account.Avatar);
+                    ImgSenderConfirm.Source = new BitmapImage(userUri);
+                }
+
+                TxtConfirmSend.Text = "Success! You sent money to " + destinationName + ".";
+
+                TxtTitle.Text = "Confirmed";
+                GrdSendMoneyInputs.Visibility = Visibility.Collapsed;
+                GrdReview.Visibility = Visibility.Collapsed;
+                GrdConfirm.Visibility = Visibility.Visible;
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        /// <summary>
+        /// Make sure all of the form fields have been entered and are valid.
+        /// </summary>
+        /// <returns>True if valid; else false</returns>
+        private bool ValidateForm()
+        {
+            bool isValid = true;
+            try
+            {
+                int pin;
+                if (string.IsNullOrEmpty(TxtPin.Password.Trim()))
+                {
+                    isValid = false;
+                    ShowInvalidPINMessage("PIN is required");
+                }
+                else if (TxtPin.Password.Trim().Length != 4)
+                {
+                    isValid = false;
+                    ShowInvalidPINMessage("PIN must be 4 digits");
+                }
+                else if (!Int32.TryParse(TxtPin.Password.Trim(), out pin))
+                {
+                    isValid = false;
+                    ShowInvalidPINMessage("Invalid PIN format");
+                }
+            }
+            catch (Exception)
+            {
+                isValid = false;
+            }
+            return isValid;
+        }
+
+        /// <summary>
+        /// Helper function which shows a message dialog box for the given message.
+        /// </summary>
+        /// <param name="message">The error message to display</param>
+        private void ShowInvalidPINMessage(string message)
+        {
+            var result = MessageBox.Show(message);
+            if (result == MessageBoxResult.OK)
+            {
+                TxtPin.Password = "";
+                TxtPin.Focus();
+            }
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        public string TransactionId { get; private set; }
+
+        #endregion
+
+    }
+}
---
+++ DwollaWP8SDKSamples/DwollaWP8SDKSamples/MainPage.xaml.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Navigation;
+using Dwolla.InAppSDK;
+using Dwolla.InAppSDK.Models;
+using Dwolla.InAppSDK.Utils;
+
+namespace DwollaWP8SDKSamples
+{
+    public partial class MainPage
+    {
+        #region Private Variables
+
+        private SendMoneyHelper _sendMoneyHelper;
+
+        #endregion
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            Loaded += MainPage_Loaded;
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        #region Page Methods
+
+        void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            ResetViews();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            Initialize();
+
+            // Check the arguments from the query string passed to the page.
+            IDictionary<string, string> uriParams = NavigationContext.QueryString;
+
+            // "Approve"
+            if (uriParams.ContainsKey(OAuthParameters.Code) && e.NavigationMode != NavigationMode.Back)
+            {
+                //Now have the temp access code. Call to get the auth token
+                var accessCode = uriParams[OAuthParameters.Code];
+                AuthenticateWithCode(accessCode);
+            }
+            // "Deny"
+            else if (uriParams.ContainsKey(OAuthParameters.Error) && e.NavigationMode != NavigationMode.Back)
+            {
+            }
+        }
+
+        private string GetResource(string key)
+        {
+            if (Application.Current.Resources != null && Application.Current.Resources.Contains(key))
+            {
+                return Application.Current.Resources[key].ToString();
+            }
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Setup the control to show the initial screen
+        /// </summary>
+        private void Initialize()
+        {
+            _sendMoneyHelper = new SendMoneyHelper(GetResource("MerchantId"), true, GetResource("AppKey"), GetResource("AppSecret"), GetResource("RedirectUri"));
+            UcSendMoney.SendMoneyCompleted += SendMoneyComplete;
+            UcSendMoney.CancelSendMoney += CloseSendMoney;
+            SetLoginStatus();
+        }
+
+        private void ResetViews()
+        {
+            SpStore.Visibility = Visibility.Visible;
+            GridStore.Visibility = Visibility.Visible;
+            SpPayNow.Visibility = Visibility.Collapsed;
+        }
+
+        /// <summary>
+        /// Event handler fired from UcSendMoney if the user cancels
+        /// </summary>
+        private void CloseSendMoney(object sender, string e)
+        {
+            ResetViews();
+        }
+
+        /// <summary>
+        /// Event handler fired from UcSendMoney when user is finished sending money (clicks done)
+        /// </summary>
+        private void SendMoneyComplete(object sender, string e)
+        {
+            //Here is the transactionId
+            string transactionId = e;
+            GetTransactionByID(transactionId);
+            ResetViews();
+        }
+
+        #endregion
+
+        #region Authentication Methods
+
+        /// <summary>
+        /// Used to set the visibility of the logout button
+        /// </summary>
+        private void SetLoginStatus()
+        {
+            if (_sendMoneyHelper != null)
+            {
+                //TxtLoginStatus.Text = _sendMoneyHelper.IsLoggedIn() ? "Logged In" : "Not Logged In";
+                BtnLogout.Visibility = _sendMoneyHelper.IsLoggedIn() ? Visibility.Visible : Visibility.Collapsed;
+            }
+        }
+
+        /// <summary>
+        /// Gets transaction details by ID.  Useful after send money is completed.
+        /// </summary>
+        /// <param name="transactionId">The transaction Id</param>
+        private async void GetTransactionByID(string transactionId)
+        {
+            if (_sendMoneyHelper != null)
+            {
+                var transaction = await _sendMoneyHelper.GetTransactionByID(transactionId);
+                TxtResponse.Text = transaction.Id;
+            }
+        }
+
+        /// <summary>
+        /// Used to show and initialize the UcSendMoney control.
+        /// </summary>
+        private void SetPayNowStatus()
+        {
+            if (_sendMoneyHelper != null)
+            {
+                SpPayNow.Visibility = _sendMoneyHelper.IsLoggedIn() ? Visibility.Visible : Visibility.Collapsed;
+                SpStore.Visibility = _sendMoneyHelper.IsLoggedIn() ? Visibility.Collapsed : Visibility.Visible;
+                GridStore.Visibility = _sendMoneyHelper.IsLoggedIn() ? Visibility.Collapsed : Visibility.Visible;
+                if (_sendMoneyHelper.IsLoggedIn())
+                {
+                    UcSendMoney.Initialize(_sendMoneyHelper, Convert.ToDouble(TxtAmount.Text.Replace("$", "")));
+                }
+            }
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            Authenticate();
+        }
+
+        /// <summary>
+        /// Authenticates the user using the SDK
+        /// </summary>
+        private async void Authenticate()
+        {
+            if (_sendMoneyHelper != null)
+            {
+                AuthenticationResponse response = await _sendMoneyHelper.AuthenticateUser();
+                if (response != null)
+                {
+                    CheckForLogin();
+                    //TxtResponse.Text = response.Message;
+                }
+            }
+        }
+
+        private async void AuthenticateWithCode(string accessCode)
+        {
+            if (_sendMoneyHelper == null)
+                return;
+
+            await _sendMoneyHelper.HandleApprove(accessCode);
+
+            CheckForLogin();
+        }
+
+        private void CheckForLogin()
+        {
+            if (_sendMoneyHelper.IsLoggedIn())
+            {
+                ////Get the balance just for fun
+                //BalanceResponse bal = await _sendMoneyHelper.GetBalance();
+                //if (bal != null)
+                //    TxtResponse.Text += "; balance: " + bal.Response;
+
+                //Authenticated so now prompt user for payment
+                SetPayNowStatus();
+            }
+
+            SetLoginStatus();
+        }
+
+        private void Logout_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (_sendMoneyHelper != null)
+            {
+                _sendMoneyHelper.Logout();
+                SetLoginStatus();
+            }
+        }
+
+        #endregion
+    }
+}
